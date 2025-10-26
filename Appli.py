5f85c185ad03544684764e5793f6a4e740f17085
azmourad067-stack <azmourad067@gmail.com>
2025-10-26 12:58:09 +0100

    Update Appli.py

--- Appli.py
+++ Appli.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 import re
 import json
 from datetime import datetime
-import plotly.express as px
+import plotly.express 
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
